70025cf949ff775a993a6d1a1c32c306af35ffaa
prajapatilalit <prajapatilalit150992@gmail.com>
2021-04-08 00:30:39 +0530

    todo initial setup

---
+++ todo/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <h1>To-Do</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
